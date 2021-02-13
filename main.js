26ca3592a49d08407713d028de46c80e5ea55299
Nazar Mahdych <77791002+mahdychnazar@users.noreply.github.com>
2021-02-13 22:55:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 $(function() {
 
     var template = $(".template").html();
-    var $blList = $(".eleven-wide");
+    var $leftSide = $(".eleven-wide");
     var $input = $(".input-form");
-    var statisticTemplate = $(".right-template").html();
-    var $statLeft = $(".segment-left");
-    var $statDone = $(".done");
-    
-    function addOneItem(name) {
+
+
+        var statTemplate = $(".right-template").html();
+        var $statLeft = $(".segment-left");
+        var $statDone = $(".done");
+        
+    function addItem(name) {
         var quantity = 1;
         var $node = $(template);
-        var $plus = $node.find(".plus-button");
-        var $minus = $node.find(".minus-button");
+
         var $blLabel = $node.find(".count-label");
-        var $delete = $node.find(".delete-button");
-        var $bought = $node.find(".buy-button");
-        var $unBought = $node.find(".notbuy-button");
+
+        var $notBuy = $node.find(".notbuy-button");
         var $productName = $node.find(".product-name");
         var $changeNameInput = $node.find(".change-input");
+
+        var $plus = $node.find(".plus-button");
+        var $minus = $node.find(".minus-button");
+        var $delete = $node.find(".delete-button");
+        var $buy = $node.find(".buy-button");
         
-        var $statNode = $(statisticTemplate);        
-        var $statProdName = $statNode.find(".product-left-name");
-        var $statProdCount = $statNode.find(".product-counter");
+        var $statNode = $(statTemplate);        
+        var $statProductName = $statNode.find(".product-left-name");
+        var $statProductCount = $statNode.find(".product-counter");
               
         $minus.attr("disabled", true);
         
@@ -29,8 +34,8 @@ $(function() {
         $productName.text(name);
         $blLabel.text(1);
         
-        $statProdName.text(name);
-        $statProdCount.text(1);
+        $statProductName.text(name);
+        $statProductCount.text(1);
 
         function buyClick() {
             $node.addClass("state-bought");
@@ -46,28 +51,31 @@ $(function() {
             $statLeft.append($statNode);
         }
         
-        $productName.click(function() {   
+        $productName.click(function() {  
+
             var $oldName = $productName.text();
             $node.addClass("state-change-name");
             $node.removeClass("state-not-change-name");  
-            var $newName = "";
-            console.log("old name", $oldName);
-            $changeNameInput.val($oldName);
+
+
+
+                var $newName = "";
+                console.log("old name", $oldName);
+                $changeNameInput.val($oldName);
         
             
             $changeNameInput.on("input", function() {
                 if ($(this).val().length > 0) {
                     console.log("this name", $(this).val());
-                    
                     $newName=$(this).val();
                      console.log("new name", $newName);
                      $productName.text($newName);
-                    $statProdName.text($newName);
+                        $statProductName.text($newName);
                 }
-                else{
-                    $productName.text($oldName);
-                     $statProdName.text($oldName);
-                }
+                     else{
+                          $productName.text($oldName);
+                             $statProductName.text($oldName);
+                    }
             });
             
             $changeNameInput.focus();        
@@ -82,7 +90,10 @@ $(function() {
             fn();
             node.fadeIn(250); });
         }
-        
+        function deleteItem() {
+            $node.remove();
+            $statNode.remove();
+        }
 
         function plusOne() {
             quantity++;
@@ -90,7 +101,7 @@ $(function() {
             $minus.attr("disabled", false);
             }
             $blLabel.text(quantity);
-            $statProdCount.text(quantity);
+            $statProductCount.text(quantity);
         }
 
         function minusOne() {
@@ -99,32 +110,29 @@ $(function() {
             $minus.attr("disabled", true);
             }
             $blLabel.text(quantity);
-            $statProdCount.text(quantity);
+            $statProductCount.text(quantity);
         }
 
-            function deleteItem() {
-                $node.remove();
-                $statNode.remove();
-            }
+           
 
-            $bought.click(buyClick);
-            $unBought.click(notBuyClick);
+            $buy.click(buyClick);
+            $notBuy.click(notBuyClick);
             $delete.click(deleteItem);
             $plus.click(plusOne);
             $minus.click(minusOne);     
         
 
-        $node.addClass("state-not-bought");
-        $node.addClass("state-not-change-name");
+            $node.addClass("state-not-bought");
+            $node.addClass("state-not-change-name");
         
-        $blList.append($node);
+     $leftSide.append($node);
         $statLeft.append($statNode);
     }
 
     $(".add-button").click(function() {
         var text = $input.val();
         if (text.length > 0) {
-            addOneItem(text);
+            addItem(text);
             $input.val("");
         }
         
@@ -136,13 +144,14 @@ $(function() {
         if (e.which == 13) {
             var text = $input.val();
             if (text.length > 0) {
-                addOneItem(text);
+                addItem(text);
                 $input.val("");
             }
         }
     });
+    
+    addItem("Помідори");
+    addItem("Печиво");
+    addItem("Сир");
 
-    addOneItem("Сир");
-    addOneItem("Бургер");
-    addOneItem("Coca-Cola");
 });
